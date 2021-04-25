5865e215446394d4e67cab701d782f72600dd67a
Alessandro Romano <alessandro.romano1992@gmail.com>
2021-04-25 22:45:41 +0200

    remove unused file

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -1,16 +1,14 @@
-import PostPreview from './post-preview'
-import Post from '../types/post'
+import PostPreview from './post-preview';
+import Post from '../types/post';
 
 type Props = {
-  posts: Post[]
-}
+  posts: Post[];
+};
 
 const MoreStories = ({ posts }: Props) => {
   return (
     <section>
-      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        More Stories
-      </h2>
+      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">More Stories</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {posts.map((post) => (
           <PostPreview
@@ -24,7 +22,7 @@ const MoreStories = ({ posts }: Props) => {
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default MoreStories
+export default MoreStories;
