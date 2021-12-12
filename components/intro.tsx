2e0cc94fb13d0103049e060e9f0517c792bd9793
Alessandro Romano <alessandro.romano1992@gmail.com>
2021-12-12 23:21:31 +0100

    adds image

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,8 +1,21 @@
+import Image from 'next/image';
+import profilePicture from '../public/assets/alepro.png';
+
 export const Intro = () => {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold md:pr-8">Alessandro Romano</h1>
-      <h2 className="text-4xl md:text-6xl italic md:pr-8">The Empathic Tech Lead</h2>
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-8 mb-8 md:mb-6">
+      <header className="mb-4 md:mb-3">
+        <h1 className="text-4xl md:text-6xl font-bold md:pr-8">Alessandro Romano</h1>
+        <p className="text-2xl md:text-3xl italic md:pr-8">The Empathic Tech Lead</p>
+      </header>
+      <Image
+        src={profilePicture}
+        alt="Alessandro Romano Profile"
+        className="rounded-full border-foreground border-4 border-dashed"
+        width={600}
+        height={400}
+        placeholder="blur"
+      />
     </section>
   );
 };
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,1 @@
-export const OG_IMAGE_URL = `https://aleromano.com/assets/blog/authors/alepro.jpg`;
+export const OG_IMAGE_URL = `https://aleromano.com/assets/blog/authors/alepro.png`;
